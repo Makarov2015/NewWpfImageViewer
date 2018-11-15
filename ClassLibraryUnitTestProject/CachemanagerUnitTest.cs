15c48d74590f7cf86a3ff346642071a37b5ded3e
Егор Макаров <makarov.e.g@outlook.com>
2018-11-15 17:54:54 +1000

    Переделочки

---
+++ ClassLibraryUnitTestProject/CachemanagerUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlbumClassLibrary.Interfaces.CacheManager;
+
+namespace ClassLibraryUnitTestProject
+{
+    [TestClass]
+    public class CachemanagerUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CacheManager manager = new CacheManager(@"C:\Users\makarov\AppData\Roaming\NewWpfImageViewer\_cache");
+        }
+    }
+}
